8f4f69cc26f2a85840eb84f737426b924161b22f
Mark Llobrera <mark@dirtystylus.com>
2024-03-06 17:00:08 -0500

    updated to ESM

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 This project will read the RSS feed of a Letterboxd user and create local Markdown files from the user’s Letterboxd diary. This is most useful for creating a local archive of your Letterboxd account (I use it for publishing my film log to my Eleventy-based website).
 
-This uses a [fork](https://github.com/dirtystylus/letterboxd) of the v2 release of the npm module [letterboxd](https://www.npmjs.com/package/letterboxd)
+This uses a [fork](https://github.com/dirtystylus/letterboxd) of the npm module [letterboxd](https://www.npmjs.com/package/letterboxd)
 
 ## Usage
 
 1. Run `npm install`.
 2. Copy **`.env.example`** to **`.env`** and set `OUTPUT_DIR` to be your desired output folder
 3. Run `npm i -g` to install the `letterboxd` command globally
-4. Run `letterboxd diary -a [account name]` with the letterboxd account name that you want to convert to Markdown
+4. Run `letterboxd diary [account name]` with the letterboxd account name that you want to convert to Markdown
 
--- bin/letterboxd.js
+++ bin/letterboxd.js
@@ -1,7 +1,44 @@
 #!/usr/bin/env node
-let args = require('yargs')
-	.commandDir('commands')
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import RSS from '../lib/diary.js';
+
+yargs(hideBin(process.argv))
+	.command({
+		command: 'diary <account>',
+		desc: 'Get the RSS feed by account name',
+		handler: (argv) => {
+			let rss = new RSS({account: argv.account});
+			rss.run();
+		}
+	})
 	.demandCommand()
 	.help()
-	.epilog('copyright 2023')
-	.argv
+	.parse()
+// let args = require('yargs')
+	
+	/*
+.command({
+	command: 'configure <key> [value]',
+	aliases: ['config', 'cfg'],
+	desc: 'Set a config variable',
+	builder: (yargs) => yargs.default('value', 'true'),
+	handler: (argv) => {
+		console.log(`setting ${argv.key} to ${argv.value}`)
+	}
+})
+
+exports.builder = (yargs) => {
+	yargs.alias('a', 'account')
+	.describe('a', 'Letterboxd account name')
+	.demandOption('a', 'Account is required, exiting.')
+	.wrap(null);
+	
+	return yargs;
+}
+
+exports.handler = (args) => {
+	let RSS = require('../../lib/diary.js');
+	let rss = new RSS(args);
+	rss.run();
+}*/
